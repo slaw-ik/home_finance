b45e82206894368457d5742871cedec162fa2563
Myroslav <slaw-ik@yandex.ru>
2015-05-18 21:22:46 +0200

    Update and Destroy sum functionality

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -14,13 +14,32 @@ class Transaction < ActiveRecord::Base
   validates :currency_id, numericality: {greater_than: 0}
   validates :transaction_type_id, presence: true
 
-  after_save :add_sum
+  after_create :create_sum
+  after_update :update_sum
+  after_destroy :destroy_sum
 
   private
-  def add_sum
-    puts "=============================================="
-    self.inspect
+  def create_sum
     Sum.add(self.amount, self.transaction_type_id, self.date, self.user_id)
   end
 
+  def update_sum
+    amount_diff = self.amount - self.amount_was
+    date_changed = (self.date != self.date_was)
+    amount_changed = (amount_diff != 0)
+
+
+    if date_changed
+      Sum.add(-self.amount_was, self.transaction_type_id, self.date_was, self.user_id)
+      Sum.add(self.amount, self.transaction_type_id, self.date, self.user_id)
+    else
+      if amount_changed
+        Sum.add(amount_diff, self.transaction_type_id, self.date, self.user_id)
+      end
+    end
+  end
+
+  def destroy_sum
+    Sum.add(-self.amount, self.transaction_type_id, self.date, self.user_id)
+  end
 end
