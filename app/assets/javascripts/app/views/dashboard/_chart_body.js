b0615e6c22fc480fd1fe863952e7d68353af2f65
Myroslav Nastenko <nastenko@essential-data.sk>
2015-07-09 12:22:37 +0200

    highcharts. In progress

--- app/assets/javascripts/app/views/dashboard/_chart_body.js
+++ app/assets/javascripts/app/views/dashboard/_chart_body.js
@@ -41,35 +41,13 @@ App.Views.ChartBody = App.Views.Base.extend({
           //  x: 0,
           //  y: 100
           //},
-          series: [{
-            series: '',
-            colorByPoint: true,
-            data: this.model.get('columns')
-          }]
-        });
-        break;
-      case 'tendency_old':
-        this.chart = c3.generate({
-          bindto: bindto,
-          data: {
-            xs: {
-              'Debet': 'debet_dates',
-              'Credit': 'credit_dates'
-            },
-            columns: this.model.get('columns'),
-            colors: {
-              'Debet': 'red',
-              'Credit': 'green'
+          series: [
+            {
+              series: '',
+              colorByPoint: true,
+              data: this.model.get('columns')
             }
-          },
-          axis: {
-            x: {
-              type: 'timeseries',
-              tick: {
-                format: '%d-%m-%Y'
-              }
-            }
-          }
+          ]
         });
         break;
       case 'tendency':
@@ -104,39 +82,22 @@ App.Views.ChartBody = App.Views.Base.extend({
 
         break;
       case 'bar':
-        this.chart = c3.generate({
-          bindto: bindto,
-          data: {
-            xs: {
-              'Debet': 'debet_dates',
-              'Credit': 'credit_dates'
-            },
-            columns: this.model.get('columns'),
-            type: 'bar',
-            colors: {
-              'Debet': 'red',
-              'Credit': 'green'
+        this.chart = bindto.highcharts({
+          chart: {
+            type: 'column'
+          },
+          xAxis: {
+            categories: this.model.get('categories')
+          },
+          plotOptions: {
+            series: {
+              allowPointSelect: false
             }
           },
-          bar: {
-            width: {
-              ratio: 0.5 // this makes bar width 50% of length between ticks
-            }
-            // or
-            //width: 100 // this makes bar width 100px
-          },
-          axis: {
-            x: {
-              type: 'timeseries',
-              tick: {
-                format: '%d-%m-%Y'
-              }
-            }
-          }
+          series: this.model.get('series')
         });
         break;
-
-      case 'bucket_state':
+      case 'bucket_state_old':
         this.chart = c3.generate({
           bindto: bindto,
           data: {
@@ -154,6 +115,23 @@ App.Views.ChartBody = App.Views.Base.extend({
               }
             }
           }
+        });
+        break;
+      case 'bucket_state':
+        this.chart = bindto.highcharts({
+          chart: {
+            type: 'area'
+          },
+          xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          },
+
+          series: [
+            {
+              data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+              color: '#FF0000'
+            }
+          ]
         });
         break;
       default:
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,9 +10,9 @@ class DashboardController < ApplicationController
         sum = 0
         transaction_type = TransactionType.find_by_name('debet')
         transactions = Transaction.includes(:category).group(:category_id)
-                           .where(:date => date_from..date_to, :transaction_type => transaction_type)
-                           .select(:category_id, "SUM(amount) as sum_amount")
-                           .collect { |trans|
+        .where(:date => date_from..date_to, :transaction_type => transaction_type)
+        .select(:category_id, "SUM(amount) as sum_amount")
+        .collect { |trans|
           sum +=trans.sum_amount
           {name: trans.category.name, y: trans.sum_amount}
         }
@@ -20,21 +20,43 @@ class DashboardController < ApplicationController
       when 'tendency'
         tendency = Sum.get_tendency(date_from, date_to)
 
-        result = {categories: tendency[:debet_sums].map { |s| s.date_from } + tendency[:credit_sums].map { |s| s.date_from },
+        debet_sums = {}
+        credit_sums = {}
+
+        tendency[:debet_sums].each { |s| debet_sums[s.date_from.to_s] = s.value }
+        tendency[:credit_sums].each { |s| credit_sums[s.date_from.to_s] = s.value }
+        dates = (debet_sums.keys | credit_sums.keys).sort
+        debets = dates.map { |d| debet_sums[d] }
+        credits = dates.map { |d| credit_sums[d] }
+
+
+        result = {categories: dates,
                   series: [{name: 'Debet',
-                            data: tendency[:debet_sums].map { |s| s.value }},
+                            data: debets},
                            {name: 'Credit',
-                            data: tendency[:credit_sums].map { |s| s.value }}
+                            data: credits}
                   ]}
 
       when 'bar'
-        debet_sums = Sum.month_debets(date_from, date_to)
-        credit_sums = Sum.month_credits(date_from, date_to)
+        d_sums = Sum.month_debets(date_from, date_to)
+        c_sums = Sum.month_credits(date_from, date_to)
 
-        result = {columns: [debet_sums.map { |s| s.date_from }.unshift('debet_dates'),
-                            credit_sums.map { |s| s.date_from }.unshift('credit_dates'),
-                            debet_sums.map { |s| s.value }.unshift('Debet'),
-                            credit_sums.map { |s| s.value }.unshift('Credit')]}
+        debet_sums = {}
+        credit_sums = {}
+
+        d_sums.each { |s| debet_sums[s.date_from.to_s] = s.value }
+        c_sums.each { |s| credit_sums[s.date_from.to_s] = s.value }
+        dates = (debet_sums.keys | credit_sums.keys).sort
+        debets = dates.map { |d| debet_sums[d] }
+        credits = dates.map { |d| credit_sums[d] }
+
+
+        result = {categories: dates,
+                  series: [{name: 'Debet',
+                            data: debets},
+                           {name: 'Credit',
+                            data: credits}
+                  ]}
       when 'bucket_state'
         if (date_to.to_date-date_from.to_date) > 60
           bucket_states = Sum.month_bucket_sates(date_from, date_to)
@@ -44,6 +66,11 @@ class DashboardController < ApplicationController
 
         result = {columns: [bucket_states.map { |s| s.date_from }.unshift('debet_dates'),
                             bucket_states.map { |s| s.value }.unshift('Bucket state')]}
+
+        result = {categories: bucket_states.map { |s| s.date_from },
+                  series: [{name: 'Bucket state',
+                            data: bucket_states.map { |s| s.value }}
+                  ]}
       else
         # type code here
     end
