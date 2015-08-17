b12d9d7a592cc610bf3e9148b8fc67eeb3933e10
Myroslav <slaw-ik@yandex.ru>
2015-08-17 23:07:31 +0200

    Transaction form caption

--- app/assets/javascripts/app/views/transactions/trnsaction_page.js
+++ app/assets/javascripts/app/views/transactions/trnsaction_page.js
@@ -15,7 +15,8 @@ App.Views.TransactionPage = App.Views.Base.extend({
 
   render: function () {
     App.Views.TransactionPage.__super__.render.apply(this, arguments);
-    var type = ((typeof aa != 'undefined') && model.type) ? model.type : 'debet';
+    //var type = ((typeof aa != 'undefined') && model.type) ? model.type : 'debet';
+    var type = 'debet';
     this.renderForm({type: type});
     this.renderTransactions({type: type});
     new Tablesort(this.$el.find('#transactions-table table')[0]);
@@ -41,16 +42,27 @@ App.Views.TransactionPage = App.Views.Base.extend({
     tab.addClass('active');
   },
 
-  renderView: function (tab, type) {
+  renderView: function (tab, type, params) {
     if (tab.attr('class') !== 'active') {
       this.activateTab(tab);
-      this.renderForm({type: type});
+      this.renderForm(_.extend({type: type}, params));
       this.renderTransactions({type: type});
     }
   },
 
   renderForm: function (params) {
+    var form_header = params.form_header || '';
     this.model = new App.Models.Transaction({type: params.type});
+
+    if (!form_header) {
+      if (params.type == 'debet') {
+        form_header = 'Додати розхід'
+      } else {
+        form_header = 'Додати дохід'
+      }
+    }
+
+    this.model.set('form_header', form_header);
     var transaction_form = new App.Views.TransactionForm({model: this.model});
 
     this.$el.find('#new-transaction').html(transaction_form.render().el);
