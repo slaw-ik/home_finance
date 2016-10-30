7ae5a8bbbe2e381c1faa141d641c6f9e14e591a7
myroslav <myroslav.nastenko@gmail.com>
2016-10-30 21:51:49 +0100

    Fix merge conflicts

--- app/assets/javascripts/app/views/dashboard/_expenditures.js
+++ app/assets/javascripts/app/views/dashboard/_expenditures.js
@@ -1,18 +1,32 @@
 /**
- * Created by myroslav on 23.9.2016.
+ * Created by myroslav on 13.08.16.
  */
 
 App.Views.Expenditures = App.Views.Base.extend({
 
-  template: HandlebarsTemplates['dashboard/expenditures'],
+    template: JST['dashboard/expenditures'],
 
-  initialize: function () {
-    App.Views.Expenditures.__super__.initialize.apply(this, arguments);
-  },
+    initialize: function () {
+        App.Views.Expenditures.__super__.initialize.apply(this, arguments);
 
-  render: function () {
-    App.Views.Expenditures.__super__.render.apply(this, arguments);
-    return this;
-  }
+        this.model = new App.Models.Report({
+            type: 'debet',
+            rangeSelector: this.rangeSelector,
+            dateFrom: this.rangeSelector.dateFrom,
+            dateTo: this.rangeSelector.dateTo,
+            title: 'Діаграма витрат за період',
+            viewId: 'my1'
+        });
 
+        this._chart = new App.Views.ExpendituresChart({model: this.model});
+    },
+
+
+    render: function () {
+        App.Views.Expenditures.__super__.render.apply(this, arguments);
+
+        this.$el.find('.chart-container').append(this._chart.render().el);
+
+        return this;
+    }
 });
